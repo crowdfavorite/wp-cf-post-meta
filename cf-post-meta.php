3bc52916a16249a47cf57834bdc3fc65e8703a67
sparker <sparker@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-02-04 22:34:40 +0000

    Bumping version number to create a new tag
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-post-meta/trunk@7652 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-post-meta.php
+++ cf-post-meta.php
@@ -3,16 +3,53 @@
 Plugin Name: CF Post Meta
 Plugin URI: http://crowdfavorite.com/wordpress/
 Description: CrowdFavorite Post Metadata Manager: Facilitates adding additinal metadata fields to posts through the standard post entry interface. 
-Version: 1.5
+Version: 1.5.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */	
 /* Tested back to PHP 4.4.7 & up to PHP 5.2.8 */
+
+ 
+// Sample config item
+
+	/*
+	$group[] = array(
+		'title' => 'Title for page block', // required
+		'description' => 'Description, inline elements',
+		'type' => array('page','post') // required, or just a string of 'page' or 'post'
+		'id' => 'unique_id', // required
+		'add_to_sortables' => bool, // whether the group is added to the sortables array or not
+		'items' => array(
+			array(
+				'name' => '_unique meta_name', // required
+				'label' => 'text for field label',
+				'label_position' => 'before/after',
+				'type' => 'checkbox/radio/int(x)/varchar(x)/text/textarea/password(x)', // required
+				'length' => array('low','high'), // min/max length for input value - not yet implemented
+				'required' => false,
+				'values' => array('optional','values','for','radio','and','check','boxes') // required for radio/checkbox
+				'before' => 'html to put before the element',
+				'after' => 'html to put after the element',
+				'wysiwyg' => bool // only valid on textarea elements, will be ignored on all others
+			),
+			array(
+				'block_id' => '_unique_id', // required
+				'block_label' => 'block title string',
+				'type' => 'block',
+				'items' => array(
+								// inputs
+							),
+				'process_group' => true // save the entire group as a serialized array, not yet implemented
+			)
+		),
+		'callback' => '! not-implemented !'
+	);
+	*/
 	
 	/**
 	 * Plugin version ID
 	 */
-	define('CF_META_VERSION', '1.5');
+	define('CF_META_VERSION', '1.5.1');
 	
 	// PHP < 4.4 hax
 	if(!defined('PHP_EOL')) { define('PHP_EOL',"\n"); }
